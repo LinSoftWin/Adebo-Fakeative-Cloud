50cdbc575b75a15d77bc2bfd052864e01fcfaaad
MiMillieuh <52078885+MiMillieuh@users.noreply.github.com>
2022-04-22 00:20:18 +0200

    Update photoshop2021install.sh

--- scripts/photoshop2021install.sh
+++ scripts/photoshop2021install.sh
@@ -41,7 +41,7 @@ touch $1/progress.mimifile
 echo "70" >> $1/progress.mimifile
 
 
-WINEPREFIX=$1/Adobe-Photoshop ./winetricks fontsmooth=rgb gdiplus msxml3 msxml6 atmlib corefonts dxvk
+WINEPREFIX=$1/Adobe-Photoshop ./winetricks fontsmooth=rgb gdiplus msxml3 msxml6 atmlib corefonts dxvk win10
 
 rm -rf $1/progress.mimifile
 touch $1/progress.mimifile
